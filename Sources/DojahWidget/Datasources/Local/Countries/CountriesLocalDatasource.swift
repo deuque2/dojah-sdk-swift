20c6ca2a48a5ea64ef42e338fd70f27d513aa7a8
Sunday <sunday.okpoluaefe@gmail.com>
2025-10-21 19:40:01 +0100

    Fix issue with realm

--- Sources/DojahWidget/Datasources/Local/Countries/CountriesLocalDatasource.swift
+++ Sources/DojahWidget/Datasources/Local/Countries/CountriesLocalDatasource.swift
@@ -9,22 +9,22 @@ import Foundation
 import RealmSwift
 
 struct CountriesLocalDatasource: CountriesLocalDatasourceProtocol {
-    private let realm = try! Realm()
-    
     func saveCountries(_ countries: [DJCountryDB]) throws {
-        try realm.save(items: countries)
-    }
-    
-    func getCountries() -> [DJCountryDB] {
-        realm.getItems()
-    }
-    
-    func getCountry(iso2: String) -> DJCountryDB? {
-        realm.get(pk: iso2)
-    }
-    
-    func getCountryByName(_ name: String) -> DJCountryDB? {
-        getCountries().first { $0.countryName.insensitiveEquals(name) }
-    }
-    
+            let realm = try Realm()
+            try realm.save(items: countries)
+        }
+
+        func getCountries() -> [DJCountryDB] {
+            let realm = try! Realm()
+            return realm.getItems()
+        }
+
+        func getCountry(iso2: String) -> DJCountryDB? {
+            let realm = try! Realm()
+            return realm.get(pk: iso2)
+        }
+
+        func getCountryByName(_ name: String) -> DJCountryDB? {
+            getCountries().first { $0.countryName.insensitiveEquals(name) }
+        }
 }
