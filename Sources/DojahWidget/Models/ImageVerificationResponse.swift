f305b8b72c06e67f1615fcf0f44dd612caa58760
Sunday <sunday.okpoluaefe@gmail.com>
2026-04-21 20:39:58 +0100

    Fixed json decode issue with image verify

--- Sources/DojahWidget/Models/ImageVerificationResponse.swift
+++ Sources/DojahWidget/Models/ImageVerificationResponse.swift
@@ -39,4 +39,31 @@ struct ImageVerificationValue: Codable {
         case url
         case confidenceValue = "confidence_value"
     }
+
+    init(url: String?, confidenceValue: String?) {
+        self.url = url
+        self.confidenceValue = confidenceValue
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        url = try container.decodeIfPresent(String.self, forKey: .url)
+
+        // Attempt to decode confidence_value as Double, Int, or String, then convert to String
+        if let doubleVal = try? container.decode(Double.self, forKey: .confidenceValue) {
+            confidenceValue = String(doubleVal)
+        } else if let intVal = try? container.decode(Int.self, forKey: .confidenceValue) {
+            confidenceValue = String(intVal)
+        } else if let stringVal = try? container.decode(String.self, forKey: .confidenceValue) {
+            confidenceValue = stringVal
+        } else {
+            confidenceValue = nil
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(url, forKey: .url)
+        try container.encodeIfPresent(confidenceValue, forKey: .confidenceValue)
+    }
 }
